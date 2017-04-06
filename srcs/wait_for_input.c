78e1eaa5a36a7fc1cf699f56cdbd478d1e32b01f
rschramm <wobula@gmail.com>
2017-04-06 16:54:58 -0700

    Normed and working with all tests in pdf.

--- srcs/wait_for_input.c
+++ srcs/wait_for_input.c
@@ -12,31 +12,38 @@
 
 #include "../includes/ft_select.h"
 
+void	key_functions(t_environment *env, unsigned long keycode)
+{
+	int refresh;
+
+	refresh = 1;
+	if (keycode == KEY_ESCAPE || ((keycode == KEY_DELETE || keycode ==
+		KEY_BACKSPACE) && env->argc == 1))
+		shutdown(env);
+	else if (keycode == KEY_UP || keycode == KEY_DOWN)
+		move_up_down(env, keycode);
+	else if (keycode == KEY_SPACE)
+		highlight_text(env);
+	else if (keycode == KEY_ENTER)
+		highlight_return(env);
+	else if (keycode == KEY_DELETE || keycode == KEY_BACKSPACE)
+		remove_element(env);
+	else
+		refresh = 0;
+	if (refresh)
+		refresh_screen(0);
+}
+
 void	wait_for_input(void)
 {
 	unsigned long	keycode;
 	t_environment	*env;
-	int				refresh;
 
-	refresh_screen(0);
 	env = store_retrieve_env(NULL);
-	while (keycode = 0, (read(0, &keycode, 6)) != 0)
+	while (1)
 	{
-		refresh = 1;
-		if (keycode == KEY_ESCAPE || ((keycode == KEY_DELETE ||
-			keycode == KEY_BACKSPACE) && env->argc == 1))
-			shutdown(env);
-		else if (keycode == KEY_UP || keycode == KEY_DOWN)
-			move_up_down(env, keycode);
-		else if (keycode == KEY_SPACE)
-			highlight_text(env);
-		else if (keycode == KEY_ENTER)
-			highlight_return(env);
-		else if (keycode == KEY_DELETE || keycode == KEY_BACKSPACE)
-			remove_element(env);
-		else
-			refresh = 0;
-		if (refresh)
-			refresh_screen(0);
+		keycode = 0;
+		if ((read(0, &keycode, 6)) != 0)
+			key_functions(env, keycode);
 	}
 }
